792b6723e119366d8493c23695d508640d0a0f65
Joachim Ruhs <postmaster@joachim-ruhs.de>
2020-05-18 08:45:31 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.3.5',
+  'version' => '1.3.6',
   'constraints' => 
   array (
     'depends' => 
