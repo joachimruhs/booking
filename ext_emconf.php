971ca3f340645b6551858f523d8f5cc7e634c11b
Joachim Ruhs <postmaster@joachim-ruhs.de>
2020-06-07 18:35:04 +0200

    Release 1.4.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.4.3',
+  'version' => '1.4.4',
   'constraints' => 
   array (
     'depends' => 
