5b733d86ba92395b1ca683e27d87d9eb830c6d8a
joachimruhs <51224372+joachimruhs@users.noreply.github.com>
2026-07-06 13:23:39 +0200

    Version 2.8.12

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -10,12 +10,18 @@ defined('TYPO3') or die();
 
 $GLOBALS['TCA']['tt_content']['types']['booking_reservation']['showitem'] = 'pi_flexform';
 
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     '*',
     'FILE:EXT:booking/Configuration/FlexForms/Booking.xml',
     'booking_reservation',
 );
 
+/*
+$GLOBALS['TCA']['tt_content']['types']['booking_reservation']['columnsOverrides']
+    ['pi_flexform']['config']['ds'] =
+    'FILE:EXT:my_extension/Configuration/FlexForms/Booking.xml';
+*/
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'tt_content',
     '--div--;Configuration,pi_flexform,pages,recursive,',
